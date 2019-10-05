abc321236ff5f3d1f62b360ffb33c3339f81dd72
github_vazquez-diego-adrian <hadiego42@gmail.com>
2019-10-04 23:03:56 -0500

    Initial commit

---
+++ app/src/main/java/avd/itics/tesoem/edu/mx/FrmPrincipalActivity.java
@@ -0,0 +1,14 @@
+package avd.itics.tesoem.edu.mx;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FrmPrincipalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frm_principal);
+    }
+}
